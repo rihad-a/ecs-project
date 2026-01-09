2a57c3694efe15744f0a8a22284cff20e5e54141
rihad-a <Rihadwork@hotmail.com>
2026-01-09 12:55:04 +0000

    Adding terraform version and tags to bootstrap provider, and adding terrraform version,tags, and the lockfile true setting

--- terraform/bootstrap/provider.tf
+++ terraform/bootstrap/provider.tf
@@ -1,9 +1,13 @@
 terraform {
+  
+  required_version = "~> 1.14.3"
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "6.27.0"
     }
+
      cloudflare = {
       source = "cloudflare/cloudflare"
       version = "~> 5.15.0"
@@ -13,4 +17,12 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+  default_tags {
+    tags = {
+      Environment = "dev"
+      Project     = "ecs"
+      Owner       = "rihad"
+      Terraform   = "true"
+    }
+  }
 }
