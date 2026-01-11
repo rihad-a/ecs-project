1783dd3007849a66ddc626f4117498214d4b4bb5
rihad-a <Rihadwork@hotmail.com>
2026-01-11 15:59:24 +0000

    Adding output for s3 bucket name and ecr registry

--- terraform/bootstrap/outputs.tf
+++ terraform/bootstrap/outputs.tf
@@ -1,9 +1,9 @@
 output "s3_bucket_name" {
   description = "Name of the S3 bucket for storing Terraform state"
-  value       = aws_s3_bucket.terraform_state.id
+  value       = aws_s3_bucket.s3.id
 }
 
 output "ecr_registry" {
   description = "URL of the Amazon ECR registry for pushing Docker images"
-  value       = aws_ecr_repository.app.repository_url
+  value       = aws_ecr_repository.ecs-project.repository_url
 }
